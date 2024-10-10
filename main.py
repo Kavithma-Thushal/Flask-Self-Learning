075a26724507ea9da775807204162d5616c0f311
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-10 13:21:25 +0530

    Main Code Refactored

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return 'Hello, Flask!'
+def home(): return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
