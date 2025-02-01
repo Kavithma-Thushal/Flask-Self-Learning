4b47182f8a2904bb230aa12a03b411e0864a09e8
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-02-01 11:54:21 +0530

    Refactored

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home(): return 'Hello World!'
+def main(): return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
